526a24f91e6c9f3fb8b616b2dd1fbb9543c52cb9
Antomadec29 <Antoinemadec29490@gmail.com>
2016-10-20 09:06:03 +0200

    Ajout add + modif files

--- Bernard/controllers/fichier.php
+++ Bernard/controllers/fichier.php
@@ -18,13 +18,53 @@ function print_file(){
 	$query=$pdo->prepare("select fichier from document group by fichier");
 	$query->execute();
 		
-	echo "<table style='width:100%'><tr><th>Fichier</th></tr>";
+	echo "<table><tr><th>Fichier</th></tr>";
 
 	for($i=0;$row=$query->fetch();$i++){
 		echo "<tr><td>".$row['fichier']."</td></tr>";
 	}
 
 	echo "</table>";
+
+}
+
+function add_file(){
+
+	$dsn='mysql:dbname=front;host=127.0.0.1';
+	$user='front';
+	$password='front';
+
+	try {
+		$pdo = new PDO($dsn,$user,$password);
+	} catch (PDOException $e) {
+		echo "Connexion échouée : ".$e->getMessage();
+	}
+
+	$fichier = $_POST['fichier'];
+
+	
+	$query=$pdo->prepare("insert into document(fichier) values('".$fichier."')");
+	$query->execute();
+
+}
+
+function modify_file(){
+
+	$dsn='mysql:dbname=front;host=127.0.0.1';
+	$user='front';
+	$password='front';
+
+	try {
+		$pdo = new PDO($dsn,$user,$password);
+	} catch (PDOException $e) {
+		echo "Connexion échouée : ".$e->getMessage();
+	}
+
+	$fichier = $_POST['fichier'];
+
+
+	$query=$pdo->prepare("update document set fichier='".$fichier."'";
+	$query->execute();
 
 }
 
