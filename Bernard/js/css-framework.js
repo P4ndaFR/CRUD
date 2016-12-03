7d344b699d3d59f49b59061e95d1fce75c8ed207
nd4pa <antoine.blondeau@isen-bretagne.fr>
2016-12-03 12:11:42 +0100

    adder nav on file view

--- Bernard/js/css-framework.js
+++ Bernard/js/css-framework.js
@@ -1,8 +1,10 @@
 $( document ).ready(function()
 {
+  $(".modify").modal();
   //$(".dropdown-button").dropdown();
   $(".button-collapse").sideNav();
   $('.collapsible').collapsible({
-      accordion : true; // A setting that changes the collapsible behavior to expandable instead of the default accordion style
-    });
+      accordion : true  // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    })
+
 });
--- Bernard/views/layout.html.php
+++ Bernard/views/layout.html.php
@@ -3,10 +3,9 @@
     <!--Import Google Icon Font-->
      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
     <link rel="stylesheet" href="css/default-materialize.css">
-    <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
+
     <meta charset="utf-8">
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
@@ -15,9 +14,8 @@
   <body>
     <!--Import jQuery before materialize.js-->
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-    <script type="text/javascript" src="js/materialize.min.js"></script>
-    <script type="text/javascript" src="js/css-framework.js"></script>
-
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
     <ul id="slide-out" class="side-nav fixed">
       <img src="../rentree/images/logo_ISEN.png" class="responsive-img"/>
       <div class=container>
@@ -51,7 +49,8 @@
       </div>
   </ul>
 
-  <?php $content ?>
+  <?php echo $content ?>
 
+  <script type="text/javascript" src="js/css-framework.js"></script>
   </body>
 </html>
