c1d612e321079b4a8ea162edec9d206a23142868
nd4pa <antoine.blondeau@isen-bretagne.fr>
2016-12-06 18:01:29 +0100

    Added FAB

--- Bernard/views/file.html.php
+++ Bernard/views/file.html.php
@@ -6,6 +6,8 @@
 		</div>
 	</div>
 </nav>
+
+<!-- Listage des fichiers -->
 <div class="container">
 	<ul class="collection">
 		<?php
@@ -53,9 +55,62 @@
 							</p>
 						</div>
 
+					</div>
+      		<a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Sauvegarder et quitter</a>
+			</form>
+    </div>
+</div>
+<div class="fixed-action-btn">
+  <a class="btn-floating btn-large red" href="#add">
+    <i class="large material-icons">add</i>
+  </a>
+</div>
 
+<!-- Ajout d'un fichier -->
+<div id="add" class="modal">
+    <div class="modal-content">
+      <h4>Ajout d'un nouveau fichier :</h4>
+    </div>
+	    <div class="modal-footer">
+				<form>
+					<div class="container">
+						<h5>Saisissez le libellé du fichier</h5>
+						<div class="">
+          		<input placeholder="" id="first_name" type="text" class="validate">
+          		<label for="file">Libellé du Fichier</label>
+        		</div>
 
+    				<div class="file-field input-field">
+      				<div class="btn">
+        				<span>File</span>
+        				<input type="file">
+      				</div>
+      				<div class="file-path-wrapper">
+        				<input class="file-path validate" type="text">
+      				</div>
+    				</div>
 
+						<h5>Cochez les promos à lier</h5>
+						<div class="row">
+							<div class="input-field inline col l2">
+            		<input placeholder="Rang" id="rang" type="number" class="validate">
+          		</div>
+							<br/>
+							<p class="col l6">
+	      				<input type="checkbox" id="test5"/>
+	      				<label for="test5">Promo 1</label>
+							</p>
+						</div>
+						<div class="row">
+							<div class="input-field inline col l2">
+            		<input placeholder="Rang" id="rang" type="number" class="validate">
+          		</div>
+							<br/>
+							<p class="col l6">
+	      				<input type="checkbox" id="test6" checked="checked"/>
+	      				<label for="test6">Promo 2</label>
+							</p>
+						</div>
 
 					</div>
       		<a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Sauvegarder et quitter</a>
