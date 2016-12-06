f2ddc88f74f60df6598fb1c5ad4c1f5890993e1a
Antomadec29 <Antoinemadec29490@gmail.com>
2016-12-06 16:58:34 +0100

    intégration du contenu des modals dans la liste

--- Bernard/views/file.html.php
+++ Bernard/views/file.html.php
@@ -9,22 +9,25 @@
 <div class="container">
 	<ul class="collection">
 		<?php
+			$i=0;
 				foreach ($fichiers as $key=>$fichier) {
 					foreach ($fichier as $key1=>$value){
 						echo "<li class='collection-item'>".$value."<a href='http://front.rentree.fr/CRUD/Bernard/?/delete_file'><i class='material-icons right'>delete</i></a>
-					<a href='#modify' class='modal-trigger'><i class='material-icons right'>reorder</i></a></li>";
+					<a href='#m".$i."' class='modal-trigger'><i class='material-icons right'>reorder</i></a></li>";
+					echo "
+						<div id='m".$i."' class='modal'>
+						    <div class='modal-content'>
+						      <h4>Modal Header</h4>
+						      <p>A bunch of text</p>
+						    </div>
+						    <div class='modal-footer'>
+						      <a href='#!' class=' modal-action modal-close waves-effect waves-green btn-flat'>Agree</a>
+						    </div>
+						</div>
+					";
+					$i++;
 					}
 				}
-			?>
+		?>
 	</ul>
-</div>
-
-<div id="modify" class="modal">
-    <div class="modal-content">
-      <h4>Modal Header</h4>
-      <p>A bunch of text</p>
-    </div>
-    <div class="modal-footer">
-      <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
-    </div>
 </div>
