3fd4af33f15590b263c3900c0a4709927a783938
nd4pa <antoine.blondeau@isen-bretagne.fr>
2016-12-13 17:00:02 +0100

    Refonte du menu, liens valides

--- Bernard/views/promos.html.php
+++ Bernard/views/promos.html.php
@@ -45,50 +45,6 @@
 	</ul>
 </div>
 
-<div id="modify" class="modal">
-    <div class="modal-content">
-      <h4>Modification du promo : <!-- ici tu met le nom du promo --></h4>
-    </div>
-	    <div class="modal-footer">
-				<form>
-					<div class="container">
-						<h5>Saisissez le libellé du promo</h5>
-						<div class="">
-          		<input placeholder="" id="first_name" type="text" class="validate">
-          		<label for="file">Libellé du promo</label>
-        		</div>
-						<h5>Cochez les promos à lier</h5>
-						<div class="row">
-							<div class="input-field inline col l2">
-            		<input placeholder="Rang" id="rang" type="number" class="validate">
-          		</div>
-							<br/>
-							<p class="col l6">
-	      				<input type="checkbox" id="test5"/>
-	      				<label for="test5">Promo 1</label>
-							</p>
-						</div>
-						<div class="row">
-							<div class="input-field inline col l2">
-            		<input placeholder="Rang" id="rang" type="number" class="validate">
-          		</div>
-							<br/>
-							<p class="col l6">
-	      				<input type="checkbox" id="test6" checked="checked"/>
-	      				<label for="test6">Promo 2</label>
-							</p>
-						</div>
-
-					</div>
-      		<a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Sauvegarder et quitter</a>
-			</form>
-    </div>
-</div>
-<div class="fixed-action-btn">
-  <a class="btn-floating btn-large red" href="#add">
-    <i class="large material-icons">add</i>
-  </a>
-</div>
 <!-- Ajout d'un promo -->
 
 <div id="add" class="modal">
