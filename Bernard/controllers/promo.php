5a3488891e136022d5286ff34dd138ab017db81c
Antomadec29 <Antoinemadec29490@gmail.com>
2016-12-28 14:43:13 +0100

    commentaire de promo.php

--- Bernard/controllers/promo.php
+++ Bernard/controllers/promo.php
@@ -3,6 +3,7 @@
 //fonction d'affichage
 function print_promo(){
 
+	//appel à la fonction de connection
 	$pdo=connect();
 
 	//requête d'affichage des fichiers
@@ -14,53 +15,65 @@ function print_promo(){
 	$promos=$query->fetchAll(PDO::FETCH_ASSOC);
 	//donner le tableau en paramètre
 	set('promos',$promos);
-	//vue avec layout
+	//revoie vers la vue avec le layout
 	return render('../views/promos.html.php','../views/layout.html.php');
 
 }
 
+//fonction d'ajout des promos
 function add_promo(){
 
+	//appel la fonction de connection
 	$pdo=connect();
 
 	//récupération des valeurs du formulaire
  	$promo=$_POST['promo'];
 	
+	//insert une promo avec un document bidon pour pouvoir l'insérer sans fichiers
 	$query=$pdo->prepare("insert into document(rang,libelle,fichier,promo) values('1','mise en base','default.txt','".$promo."')");
 		
 	//execution
 	$query->execute();	
 	
-
+	//redirection vers l'affichage des promos
 	redirect_to('/promos');
 }
 
+//fonction de modification des promos
 function modify_promo(){
 
+	//appel la fonction de connection
 	$pdo=connect();
 
+	//Recupération des valeurs du formulaire
 	$promo = $_POST['promo'];
 	$oldpromo = $_POST['oldpromo'];
 
+	//mise à jour du nom de la promotion
 	$query=$pdo->prepare("update document set promo='".$promo."' where promo='".$oldpromo."'");
+	//execution
 	$query->execute();
 	
+	//redirection vers l'affichage des promos
 	redirect_to('/promos');
 
 }
 
+//fonction de suppression des promos
 function delete_promo(){
 
+	//appel la fonction de connection
 	$pdo=connect();
 
-	//récupération de la valeur de la liste sélectionner
-	//pb récupération doit être faite par rapport à la ligne
+	//récuupération des valeurs du formulaire
 	$promo = $_GET['promo'];
 
-
+	//requête de suppression des documents ayant la promo demander
 	$query=$pdo->prepare("delete from document where promo='".$promo."'");
+	//execution
 	$query->execute();
 
+	//redirection vers l'affichage des promos
 	redirect_to('/promos');
 }
 
